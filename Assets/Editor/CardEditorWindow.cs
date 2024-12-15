2b74e9500685fa1f23a0bdd64868f801629cb487
ASGrincewicz <AaronSG@outlook.com>
2024-12-15 16:36:01 -0500

    Created Interface for Custom Editor Window, and Editable Assets.

---
+++ Assets/Editor/CardEditorWindow.cs
@@ -0,0 +1,68 @@
+using Interfaces;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    public class CardEditorWindow : EditorWindow, ICustomEditorWindow
+    {
+        private static EditorWindow _window;
+        private static string Title { get; set; } = "Card Editor";
+
+        IEditableAsset ICustomEditorWindow.LoadedAsset { get; set; }
+
+        Rect ICustomEditorWindow.MainAreaRect { get; set; }
+
+        Vector2 ICustomEditorWindow.ScrollPosition { get; set; }
+
+        [MenuItem("Card Game Tools/Card Editor")]
+        public static void Init()
+        {
+            _window = GetWindow<CardEditorWindow>(Title);
+        }
+
+        void ICustomEditorWindow.OnEnable()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        void ICustomEditorWindow.OnDisable()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        void ICustomEditorWindow.OpenInEditor(IEditableAsset asset)
+        {
+            throw new System.NotImplementedException();
+        }
+        void ICustomEditorWindow.OnGUI()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        void ICustomEditorWindow.SetupAreaRects()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        void ICustomEditorWindow.DrawMainArea()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        void ICustomEditorWindow.DrawEditableFields()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        void ICustomEditorWindow.DrawControlButtons()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        void ICustomEditorWindow.LoadAssetData(IEditableAsset asset)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
